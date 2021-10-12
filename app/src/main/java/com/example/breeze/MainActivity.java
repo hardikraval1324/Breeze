83dec9a57a815a2743ec3ab62d16daad4fe799c9
Samarth Shah <57283918+samarth-shah@users.noreply.github.com>
2021-10-12 12:51:11 +0530

    LogIn and SignUp with Firebase Database Done (#2)
    * Firebase Done
    
    * added some functionalities

--- app/src/main/java/com/example/breeze/MainActivity.java
+++ app/src/main/java/com/example/breeze/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.breeze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.example.breeze.activities.HomeActivity;
+import com.example.breeze.activities.LoginActivity;
+import com.example.breeze.activities.RegistrationActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        if (auth.getCurrentUser() != null) {
+            progressBar.setVisibility(View.VISIBLE);
+            Toast.makeText(this, "Please wait you are already logged in!", Toast.LENGTH_SHORT).show();
+
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                }
+            }, 3000);
+        }
+    }
+    public void login(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void registration(View view) {
+        startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
     }
 }
