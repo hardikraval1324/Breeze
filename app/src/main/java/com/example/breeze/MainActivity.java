b7c11d34715145837a5c450178cb4f4735cc9bc4
hardikraval1324 <hardik1324@gmail.com>
2021-10-12 13:01:00 +0530

    added welcome page

--- app/src/main/java/com/example/breeze/MainActivity.java
+++ app/src/main/java/com/example/breeze/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.breeze;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.executor.TaskExecutor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView t1;
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = findViewById(R.id.breeze);
+        go = findViewById(R.id.lesgo);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, welcome_user.class);
+                startActivity(intent);
+            }
+        });
     }
 }
