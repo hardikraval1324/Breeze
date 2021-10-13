5423304c7fe6dab4d6907220170a46e2c3156bf4
Samarth Shah <57283918+samarth-shah@users.noreply.github.com>
2021-10-13 20:00:50 +0530

    UI/UX change (#3)
    * Firebase Done
    
    * added some functionalities
    
    * keyboard hide feature
    
    * UI/UX change
    
    * UI/UX change

--- app/src/main/java/com/example/breeze/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/breeze/activities/RegistrationActivity.java
@@ -1,9 +1,12 @@
 package com.example.breeze.activities;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -52,12 +55,14 @@ public class RegistrationActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeKeyboard();
                 startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
             }
         });
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeKeyboard();
                 createUser();
                 progressBar.setVisibility(View.VISIBLE);
             }
@@ -104,4 +109,20 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void closeKeyboard()
+    {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+
+            InputMethodManager manager
+                    = (InputMethodManager)
+                    getSystemService(
+                            Context.INPUT_METHOD_SERVICE);
+            manager
+                    .hideSoftInputFromWindow(
+                            view.getWindowToken(), 0);
+        }
+    }
+
 }
